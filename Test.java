e3583f5bb5259337d9758850c0e1bb2bf6258a91
HongYingFang <zx04785@gmail.com>
2016-05-07 22:40:46 +0800

    Create Test.java
    This is the test class  :
    Let the user 
    1.enter two numbers
    2,choose a arithmetic,and then call the method from number class

---
+++ Test.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Test {
+	public static void main(String[] args) {
+		//new number
+		Number number = new Number();
+		
+		//new scanner
+		Scanner input = new Scanner(System.in);
+		
+		//Let the user enter two numbers(String type) 
+		System.out.println("Please enter two numbers.");
+		System.out.println("First number:");
+		String num1 = input.nextLine();
+		System.out.println("Second number:");
+		String num2 = input.nextLine();
+		
+		//Let the user choose a arithmetic
+		System.out.println("What kind of calculation do you want ? ");
+		System.out.println("1)add 2)subtract 3)multiply 4)divide");
+		int cal = input.nextInt();
+		
+		if(cal==4){
+		System.out.println("Enter the number for the digit you want to retain after dot.");
+		int scale = input.nextInt();
+		//divide method
+		number.div(scale,cal,num1,num2);
+		}else{		
+		//promote method
+		number.promote(cal,num1,num2);
+		}
+	}
+}
